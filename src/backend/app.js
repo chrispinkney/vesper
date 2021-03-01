0cad32ddffa5a5a07dde0be3100148f9d63a7089
Chris Pinkney <hey@chrispinkney.com>
2021-03-01 17:16:18 -0500

    initial commit

---
+++ src/backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const indexRouter = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Backend started on port ${PORT}`));
+
+module.exports = app;
